8f57bc3e82f8ee4b06aa162c41b2015f1f2160ec
georgytom98 <162505269+georgytom98@users.noreply.github.com>
2024-08-10 04:17:19 -0500

    Enable image field for plant detail view

--- app/plant/serializers.py
+++ app/plant/serializers.py
@@ -85,7 +85,8 @@ class PlantDetailSerializer(PlantSerializer):
     """Serializer for plant detail view."""
 
     class Meta(PlantSerializer.Meta):
-        fields = PlantSerializer.Meta.fields + ['description']
+        fields = PlantSerializer.Meta.fields + ['description',
+                                                'image']
 
 
 class PlantImageSerializer(serializers.ModelSerializer):
